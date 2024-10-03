d58a02a752bca827fa77430e122228bf9ef4db7a
Daniel Berg <danieltberg@hotmail.com>
2024-10-03 10:40:11 -0300

    Ciração da parte superior do site, precisa ainda bucsar uma logo

--- angular.ts
+++ angular.ts
@@ -1,15 +1,35 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root'
   template: `
-    Hello
-  `,
-  styles: `
-    :host {
-      color: blue;
+  <div class="header">
+    <div class="site-name">
+      {{ title }}
+    </div>
+    <div class="site-symbol">
+      <img src="Logo.png" alt="Logo do site">
+    </div>
+    <div class="button-group">
+      <button>Login</button>
+      <button>Registro</button>
+    </div>
+  </div>
+  `
+  styles: [`
+    .header {
+      background-color: #f0f0f0;
+      padding: 20px;
+      text-align: left; 
     }
-  `,
-  standalone: true,
+
+    .site-name {
+      font-size: 20px;
+      font-weight: bold;
+    }
+    `]
+
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Universo da 7 arte'
+}
